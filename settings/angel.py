f5d27f653845449115c822d93867064fa98caab8
Ángel Jiménez <angeljimenezgong@gmail.com>
2018-09-25 18:22:59 +0200

    First settings and requirements

---
+++ settings/angel.py
@@ -0,0 +1,29 @@
+from settings.base import *
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost', 'localhost:8000',]
+ALLOWED_HOSTS = ['127.0.0.1', '127.0.0.1:8000', 'localhost', 'localhost:8000',]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recommender',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+        'TEST': {
+            'CHARSET': 'utf8',
+            'COLLATION': 'utf8_general_ci',
+        },
+    }
+}
